416c929cb87806b88dd5925a25219b08f87fc395
none <none>
2024-12-10 19:39:13 +0000

    enhance

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # selected-window-bleach
-Not selected windows shadowed by reducing difference between foreground and background.
+Change background of not selected windows by shadowing them by calculating difference between foreground text and background brightness.
+
+Allow to adjust brightness of text and background for selected and not selected window.
+
+Allow to increase constrast between text and background.
+
+# Usage
+```Elisp
+(add-hook 'buffer-list-update-hook 'selected-window-bleach-highlight-selected-window)
+```
+
+Recommended: ```(set-face-attribute 'mode-line-active nil :background "cyan4")```
+
+# Original idea:
+```Elisp
+(defun highlight-selected-window ()
+  "Highlight selected window with a different background color."
+  (walk-windows (lambda (w)
+                  (unless (eq w (selected-window))
+                    (with-current-buffer (window-buffer w)
+                      (buffer-face-set '(:background "#111"))))))
+  (buffer-face-set 'default))
+(add-hook 'buffer-list-update-hook 'highlight-selected-window)
+```
+from https://emacs.stackexchange.com/questions/24630/is-there-a-way-to-change-color-of-active-windows-fringe
--- selected-window-bleach.el
+++ selected-window-bleach.el
@@ -1,4 +1,4 @@
-;;; selected-window-bleach.el --- Not selected windows shadowed by reducing difference between foreground and background.   -*- lexical-binding: t -*-
+;;; selected-window-bleach.el --- Accurate reduce contrast of non selected windows.   -*- lexical-binding: t -*-
 
 ;; Copyright (c) 2024 github.com/Anoncheg1,codeberg.org/Anoncheg
 
@@ -26,9 +26,23 @@
 ;;; Commentary:
 ;; Usage:
 ;; (add-hook 'buffer-list-update-hook 'selected-window-bleach-highlight-selected-window)
-;; Recommended:
-;; (set-face-attribute 'mode-line-active nil :background "cyan4")
+;; To increase contrast of selected modeline:
+;; (selected-window-bleach-change-modeline 0.7 0.7)
+;; Customize:
 ;;; Code:
+
+;; - configurable:
+(defcustom selected-window-bleach-selected-magnitude-text 1
+  "Change in contrast for selected window.
+Higher value decrease contrast between text and background.
+This value change contrast of text regarding to background.")
+(defcustom selected-window-bleach-selected-magnitude-background 1
+  ".")
+(defcustom selected-window-bleach-not-sel-magnitude-text 1.2
+  "Change in contrast for not selected window.")
+(defcustom selected-window-bleach-not-sel-magnitude-background 1.2
+  "This value change contrast of background regarding to text.")
+
 
 (defun selected-window-bleach--get-current-colors ()
   "Get the current text and background colors."
@@ -36,32 +50,34 @@
         (background-color (face-attribute 'default :background (selected-frame))))
     (list text-color background-color)))
 
-(defun selected-window-bleach--hex-to-rgb (hex)
+(defun selected-window-bleach--hex-to-hsl (hex)
   "Convert hex to RGB. Generated with AI."
   (let ((rgb (color-name-to-rgb hex)))
-    (list (/ (nth 0 rgb) 65535.0) (/ (nth 1 rgb) 65535.0) (/ (nth 2 rgb) 65535.0))))
+    (color-rgb-to-hsl (/ (nth 0 rgb) 65535.0)
+                      (/ (nth 1 rgb) 65535.0)
+                      (/ (nth 2 rgb) 65535.0))))
 
-(defvar selected-window-bleach-change-magnitude-text 1.2)
-(defvar selected-window-bleach-change-magnitude-background 1.2)
-
-(defun selected-window-bleach--adjust-brightness (text-color background-color)
-  "Adjust the brightness of the text and background colors to be closer.
-Generated with AI."
-  (let ((text-hsl (color-rgb-to-hsl
-                   (nth 0 (selected-window-bleach--hex-to-rgb text-color))
-                   (nth 1 (selected-window-bleach--hex-to-rgb text-color))
-                   (nth 2 (selected-window-bleach--hex-to-rgb text-color))))
-        (background-hsl (color-rgb-to-hsl
-                         (nth 0 (selected-window-bleach--hex-to-rgb background-color))
-                         (nth 1 (selected-window-bleach--hex-to-rgb background-color))
-                         (nth 2 (selected-window-bleach--hex-to-rgb background-color)))))
+(defun selected-window-bleach--adjust-brightness (text-color background-color magnitude-text magnitude-back)
+  "Adjust the brightness of the text and background colors.
+To be closer By the magnitude.
+Return (foreground , background)."
+  (let ((text-hsl (selected-window-bleach--hex-to-hsl text-color))
+        (background-hsl (selected-window-bleach--hex-to-hsl background-color)))
     (let ((text-brightness (nth 2 text-hsl))
           (background-brightness (nth 2 background-hsl)))
+      ;; new-text-brightness = average + (average - background) / magnitude-text
+      ;; new-back-brightness = average + (average - text) / magnitude-back
       (let ((average-brightness (/ (+ text-brightness background-brightness) 2)))
-        (let ((new-text-brightness (+ average-brightness (/ (- average-brightness background-brightness) selected-window-bleach-change-magnitude-text)))
-              (new-background-brightness (- average-brightness (/ (- text-brightness average-brightness) selected-window-bleach-change-magnitude-background))))
-          (list (color-hsl-to-rgb (nth 0 text-hsl) (nth 1 text-hsl) new-text-brightness)
-                (color-hsl-to-rgb (nth 0 background-hsl) (nth 1 background-hsl) new-background-brightness)))))))
+        (let ((new-text-brightness (+ average-brightness (/ (- average-brightness background-brightness)
+                                                            magnitude-text)))
+              (new-background-brightness (- average-brightness (/ (- text-brightness average-brightness)
+                                                                  magnitude-back))))
+          (list (color-hsl-to-rgb (nth 0 text-hsl)
+                                  (nth 1 text-hsl)
+                                  new-text-brightness)
+                (color-hsl-to-rgb (nth 0 background-hsl)
+                                  (nth 1 background-hsl)
+                                  new-background-brightness)))))))
 
 (defun selected-window-bleach--rgb-to-hex (red green blue)
   "Convert RGB to hex."
@@ -69,29 +85,66 @@ Generated with AI."
 
 (defun selected-window-bleach--apply-new-colors (text-color background-color)
   "Apply the new text and background colors."
-  (let ((fg (selected-window-bleach--rgb-to-hex (nth 0 text-color) (nth 1 text-color) (nth 2 text-color)))
-        (bg (selected-window-bleach--rgb-to-hex (nth 0 background-color) (nth 1 background-color) (nth 2 background-color))))
+  (let ((fg (apply 'selected-window-bleach--rgb-to-hex text-color))
+        (bg (apply 'selected-window-bleach--rgb-to-hex background-color)))
     (buffer-face-set (list :background bg :foreground fg))
     ))
 
-(defun selected-window-bleach-adjust-colors ()
+(defun selected-window-bleach-change (magnitude-text magnitude-back)
   "Adjust the text and background colors to be closer in brightness."
   (let* ((current-colors (selected-window-bleach--get-current-colors))
-        (new-colors (selected-window-bleach--adjust-brightness (nth 0 current-colors) (nth 1 current-colors))))
-    (selected-window-bleach--apply-new-colors (nth 0 new-colors) (nth 1 new-colors))))
+        (new-colors (selected-window-bleach--adjust-brightness (nth 0 current-colors)
+                                                               (nth 1 current-colors)
+                                                               magnitude-text
+                                                               magnitude-back)))
+    (selected-window-bleach--apply-new-colors (nth 0 new-colors)
+                                              (nth 1 new-colors))))
+
+(defun selected-window-bleach-change-modeline (magnitude-text magnitude-back)
+  "Adjust modeline brightness of text and background."
+  (let* ((backgound (face-attribute 'mode-line :background))
+         (foreground (face-attribute 'mode-line :foreground))
+         (new-colors (selected-window-bleach--adjust-brightness foreground
+                                                                backgound
+                                                                magnitude-text
+                                                                magnitude-back))
+         (new-foreground (apply 'selected-window-bleach--rgb-to-hex (nth 0 new-colors)))
+         (new-background (apply 'selected-window-bleach--rgb-to-hex (nth 1 new-colors))))
+    (set-face-attribute 'mode-line-active nil
+                        :foreground new-foreground
+                        :background new-background)))
 
 
 (defun selected-window-bleach-highlight-selected-window ()
-  "Highlight selected window with a different background color."
+  "Highlight not selected windows with a different background color."
   (let ((cbn (buffer-name (current-buffer)))
         (sw (selected-window)))
     (when (/= (aref cbn 0) ?\s) ; ignore system buffers
+      ;; - not selected:
       (walk-windows (lambda (w)
                       (unless (or (eq sw w)
                                   (eq cbn  (buffer-name (window-buffer w))))
                         (with-selected-window w
-                          ;; (print cbn)
-                          (selected-window-bleach-adjust-colors)))))
-      (buffer-face-set 'default))))
+                          (if (not (and (= selected-window-bleach-selected-magnitude-text 1)
+                                        (= selected-window-bleach-selected-magnitude-background 1)))
+                              (selected-window-bleach-change
+                               selected-window-bleach-not-sel-magnitude-text
+                               selected-window-bleach-not-sel-magnitude-background)
+                            ;; else
+                            (buffer-face-set 'default))))))
+
+      ;; - selected:
+      (if (not (and (= selected-window-bleach-selected-magnitude-text 1)
+                    (= selected-window-bleach-selected-magnitude-background 1)))
+          (progn
+            (selected-window-bleach-change
+             selected-window-bleach-selected-magnitude-text
+             selected-window-bleach-selected-magnitude-background)
+
+            (set-face-attribute 'mode-line-active nil :background "cyan4"))
+        ;; else
+        (buffer-face-set 'default)
+        ))))
+
 (provide 'selected-window-bleach)
 ;;; selected-window-bleach.el ends here
